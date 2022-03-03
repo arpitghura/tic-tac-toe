8676960553f081dc3d6f9784abcc019fdd4778ee
Arpit Ghura <ghurarpit110@gmail.com>
2022-03-03 20:31:34 +0530

    [bug fixed] game now stops after winning

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let turn = "X";
 let info = document.getElementById("user-turn")
 let gameover = false;
-let gamedraw = false;
+// let gamedraw = false;
 
 const changeTurn = () =>{
     return turn === "X"?"O":"X";
@@ -12,12 +12,13 @@ const checkWin = () =>{
     let wins = [
         [0,1,2],[3,4,5],[6,7,8],
         [0,3,6],[1,4,7],[2,5,8],
-        [2,4,8],[2,4,6]
+        [0,4,8],[2,4,6]
     ]
     wins.forEach(e=>{
         if((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[1]].innerText === boxtexts[e[2]].innerText) && (boxtexts[e[0]].innerText !== '')){
             info.innerText = boxtexts[e[0]].innerText + " Won"
             gameover=true;
+            // gamedraw=false;
             boxtexts[e[0]].parentElement.style.backgroundColor = "green";
             boxtexts[e[1]].parentElement.style.backgroundColor = "green";
             boxtexts[e[2]].parentElement.style.backgroundColor = "green";
@@ -29,13 +30,14 @@ let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(element => {
     element.addEventListener("click",()=>{
         let boxText = element.querySelector(".box-text");
-        if(boxText.innerText === ""){
-            boxText.innerText = turn;
-            turn = changeTurn();
-            // checkDraw();
-            checkWin();
-            if(!(gameover || gamedraw))
+        if(!gameover){
+            if(boxText.innerText === ""){
+                boxText.innerText = turn;
+                turn = changeTurn();
+                // checkDraw();
+                checkWin();
                 info.innerText = " Turn for "+ turn;
+            }
         }
     })
 });
@@ -48,14 +50,15 @@ const reset = ()=>{
         gameover = false
         info.innerText = " Turn for "+ turn;
         e.parentElement.style.backgroundColor = "white";
+        // gamedraw=false;
     })
 }
 
-const checkDraw = ()=>{
-    let boxtexts = document.getElementsByClassName("box-text");
-    Array.from(boxtexts).forEach(e=>{
-       if (e.innerText !== "") {
-            info.innerText = "Draw";
-       }
-    })
-}
+// const checkDraw = ()=>{
+//     let boxtexts = document.getElementsByClassName("box-text");
+//     Array.from(boxtexts).forEach(e=>{
+//        if (e.innerText !== "") {
+            
+//        }
+//     })
+// }
